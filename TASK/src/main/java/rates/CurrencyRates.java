330234325ac924565b8aa8832761b679fdf3370c
cabdiasismohamed <cabdiasismohamed15@gmail.com>
2025-03-30 15:57:40 +0300

    Commit #1
    In the NV on lines 9, 10, 11, 12, and 13 i have Initialized CurrencyRates with predefined exchange rates for USD to EUR, SSP, SOS, and UGX.

--- TASK/src/main/java/rates/CurrencyRates.java
+++ TASK/src/main/java/rates/CurrencyRates.java
@@ -5,4 +5,11 @@ import java.util.Map;
 
 public class CurrencyRates {
     private static final Map<String, Double> rates = new HashMap<>();
+
+    static {
+        rates.put("USD_EUR", 0.85);
+        rates.put("SSP", 130.0);
+        rates.put("SOS", 570.0);
+        rates.put("UGX", 3800.0);
+    }
 }
